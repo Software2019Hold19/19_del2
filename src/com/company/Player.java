b690d7b45ea8b8d3bcc2c2dac1da24fc1858af95
Jacob Berg Eriksen <jacob1211@hotmail.com>
2019-10-23 16:15:28 +0200

    Added constructor to player class

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class Player {
+    String name;
+    Account acc = new Account();
+
+    public Player (String name, int dieSiza){
+        this.name = name;
+        Die die1 = new Die(dieSiza);
+        Die die2 = new Die(dieSiza);
+    }
+
 }
