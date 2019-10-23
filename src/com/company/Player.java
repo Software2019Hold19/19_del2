273c24bb776656dcfec98971206ae1b322f937a1
Jacob Berg Eriksen <jacob1211@hotmail.com>
2019-10-23 16:26:11 +0200

    Added constructor to player class (spelling error fixed)

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,10 +4,10 @@ public class Player {
     String name;
     Account acc = new Account();
 
-    public Player (String name, int dieSiza){
+    public Player (String name, int dieSize){
         this.name = name;
-        Die die1 = new Die(dieSiza);
-        Die die2 = new Die(dieSiza);
+        Die die1 = new Die(dieSize);
+        Die die2 = new Die(dieSize);
     }
 
 }
