f69cfcfefa6daf7f95012ebc77a8f2d5f4a792d5
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2019-10-23 15:41:41 +0200

    Initializing constructor for Account"

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class Account {
+    private int balance = 1000;
+
+    public Account(){
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int val) { // FIXME: 23-10-2019 Cannot go below 0!!
+        this.balance += val;
+    }
 }
