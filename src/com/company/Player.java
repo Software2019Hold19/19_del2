de4303ec44a22eff3ed0a772799c22105c0db92c
Jacob Berg Eriksen <jacob1211@hotmail.com>
2019-10-28 10:03:29 +0100

    Text files pretty much completed

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -4,13 +4,13 @@ public class Player {
     String name;
     Account acc = new Account();
 
-    public Player (String name, int dieSize){
+    public Player (String name){
         this.name = name;
-        Die die1 = new Die(dieSize);
-        Die die2 = new Die(dieSize);
+        Die die1 = new Die();
+        Die die2 = new Die();
     }
     
-    public int[] roll(){ // FIXME: 25-10-2019 Make this use the 2 Die objects to roll 2 random numbers between 1 and dieSize
+    public int[] roll(){ // FIXME: 25-10-2019 Make this use the 2 Die objects to roll 2 random numbers between 1 and 6
         
         return ([1, 2])
     }
