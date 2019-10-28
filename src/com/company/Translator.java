06c36bafc0c542c8e73dbf7de5fbe678bb21bd3e
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2019-10-28 13:30:10 +0100

    working on Translator

--- src/com/company/Translator.java
+++ src/com/company/Translator.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Translator {
-    String fileDansk = "Language_dansk.txt";
-    String fileEngelsk = "Language_english.txt";
     String language;
     ArrayList<String> txts = new ArrayList<String>(18);
 
@@ -19,12 +17,14 @@ public class Translator {
     }
 
     private void getLanguage(String language) throws IOException { // FIXME: 25-10-2019 Will get info on how to do dis on Monday
-        String file ="minFil.txt";
+        String file ="Languages/Language_" + language.toLowerCase() + ".txt";
         BufferedReader reader = new BufferedReader(new FileReader(file));
 
         String line;
+        int count = 0;
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
+            this.txts.set(count, line);
         }
         reader.close();
     }
