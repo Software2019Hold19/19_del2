b69caf6b5526c65d09a828a5e0abd7f566dfd312
Oliver Comprés Poulsen <oli@live.dk>
2019-10-28 09:33:48 +0100

    Add init method to main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 
 public class Main {
 
+    static GUI board = new GUI();
+
     private static void playerTurn(Player player, Field[] fieldList){
        // int[] roll = player.roll();
         //int sum = roll[0] + roll[1];
@@ -10,9 +12,19 @@ public class Main {
 
 
     }
-    
+
+    private static void initGame(Field[] fields) {
+
+        int count = 1;
+        for (Field f : fields) {
+            board.setFieldText(count, f.name);
+            board.setFieldText(count, "" + f.val);
+            count++;
+        }
+    }
+
     private static void startGame(){ // FIXME: 25-10-2019 init players, fields and GUI
-       GUI board = new GUI();
+
   /*      Player p = new Player("Oliver", 6);
         board.setDice(1,2);
         board.addPlayerToBoard(p);
@@ -24,8 +36,10 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	    //Field[] fieldList;
+	    Field[] fieldList;
         //System.out.println(String.format("this is %ss saying hi to %s", "Frederik", "Jacob"));
+
+
         startGame();
 
     }
