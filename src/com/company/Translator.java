320f3dc5d2b9b2696d9abd182f2e413a512e0fbe
Frederik Schrøder Koefoed <frederikkoefoeds@gmail.com>
2019-10-28 13:20:45 +0100

    working on translator

--- src/com/company/Translator.java
+++ src/com/company/Translator.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Translator {
@@ -12,7 +15,14 @@ public class Translator {
         getLanguage(language);
     }
 
-    private void getLanguage(String language){ // FIXME: 25-10-2019 Will get info on how to do dis on Monday
+    private void getLanguage(String language) throws FileNotFoundException { // FIXME: 25-10-2019 Will get info on how to do dis on Monday
+        String file ="minFil.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(file));
 
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line);
+        }
+        reader.close();
     }
 }
