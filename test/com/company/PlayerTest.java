9b52153d0c49749ab1c3def791440c3e558eb50c
Dánjal Dávidsson <s195472>
2019-10-30 13:29:55 +0100

    Moved testRoll() from PlayerTest to DieTest and fix minor mistakes in test code

--- test/com/company/PlayerTest.java
+++ test/com/company/PlayerTest.java
@@ -17,23 +17,4 @@ public class PlayerTest {
         Player player = new Player("name");
         assertEquals("name", player.guiPlayerObj.getName());
     }
-
-    @Test
-    public void testRoll() {
-        int min = 0; int max = 7; int rollNum = 10000;
-
-        Player player = new Player("name");
-        int[] dice = player.roll();
-        boolean limit = true;
-
-        for(int count = 0; count < rollNum; count++){
-            for(int i = 0; i < dice.length; i++){
-                if(dice[i] < min || dice[i] > max){
-                    limit = false;
-                    break;
-                }else if(!limit){ break; }
-            }
-        }
-        assertTrue(limit);
-    }
 }
